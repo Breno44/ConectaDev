d7be9dbb56ae25fa57425c4f15dd482fa50655f0
Breno Henrique <bhfs.2908@gmail.com>
2021-08-18 18:03:40 -0300

    feat:add logo

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles({
+  appBar: {
+    boxShadow: 'none',
+  },
+});
 
 export function Header() {
+  const classes = useStyles();
+
   return (
-    <header className="header">
-      <div className="toolbar">
-        <div className="">
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
+      <Toolbar>
+        <img src="/images/logo.png" alt="logo" />
+        {/* <div className="">
           <span>Conecta Dev</span>
         </div>
 
         <div className="">
-          <button>Novo Post</button>
+          <Button color="primary" variant="contained">
+            Novo Post
+          </Button>
           <span>Img1</span>
           <span>Img2</span>
-        </div>
-      </div>
-    </header>
+        </div> */}
+      </Toolbar>
+    </AppBar>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import './styles.css';
+// import './styles.css';
 import { Header } from './components/Header';
+import { NavBar } from './components/NavBar';
+import { Feed } from './components/Feed';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  main: {
+    height: '100vh',
+    display: 'flex',
+    width: '1200px',
+    margin: '0 auto',
+  },
+});
 
 export function Home() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <Header />
-      <main className="main">
-        <div className="navbar"></div>
-        <div className="feed"></div>
+      <div className="toolbar"></div>
+      <main className={classes.main}>
+        <NavBar />
+        <Feed />
       </main>
     </div>
   );
