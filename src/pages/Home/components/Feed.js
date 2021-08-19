3da97a20757118298caa68f72b555715b3864164
Breno Henrique <bhfs.2908@gmail.com>
2021-08-19 18:11:01 -0300

    feat:component feed

--- src/pages/Home/components/Feed.js
+++ src/pages/Home/components/Feed.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { PostCard } from '../../../components/PostCard/index';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      id: 1,
+      name: 'Alex araujo',
+      username: 'Alex123',
+      avatar: '/images/avatars/avatar_1.jpeg',
+    },
+    title: 'Criando um app do zero usando react.js',
+    date: 'April 7, 2020',
+    description: 'fala pessoal qual o framework favorito de vcs?',
+    hashtags: '#javascript, #react, #frontend',
+    image: '/images/posts/post9.jpeg',
+  },
+  {
+    id: 2,
+    author: {
+      id: 1,
+      name: 'Alex araujo',
+      username: 'Alex123',
+      avatar: '/images/avatars/avatar_1.jpeg',
+    },
+    title: 'Comparativo entre react.js e angular.js',
+    date: 'April 1, 2020',
+    description: 'fala pessoal qual o framework favorito de vcs?',
+    hashtags: '#javascript, #react, #frontend, #angular',
+    image: '/images/posts/post8.png',
+  },
+];
 
 export function Feed() {
-  return <div>feed</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, SvgIcon } from '@material-ui/core';
+import { Bell } from 'react-feather';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles({
   appBar: {
     boxShadow: 'none',
+  },
+  img: {
+    maxHeight: 55,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  userSelection: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  button: {
+    marginRight: 10,
+  },
+  bell: {
+    marginRight: 10,
   },
 });
 
@@ -17,7 +35,17 @@ export function Header() {
   return (
     <AppBar position="fixed" color="inherit" className={classes.appBar}>
       <Toolbar>
-        <img src="/images/logo.png" alt="logo" />
+        <img src="/images/logo.png" alt="logo" className={classes.img} />
+        <div className={classes.grow}></div>
+        <div className={classes.userSelection}>
+          <Button color="primary" variant="contained" className={classes.button}>
+            Novo post
+          </Button>
+          <SvgIcon className={classes.bell}>
+            <Bell></Bell>
+          </SvgIcon>
+          <Avatar alt="Breno" src="/" />
+        </div>
         {/* <div className="">
           <span>Conecta Dev</span>
         </div>
