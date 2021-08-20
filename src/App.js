9f9898eed23ed2f8b80b780787f7382a3be15816
Breno Henrique <bhfs.2908@gmail.com>
2021-08-20 16:29:03 -0300

    feat:add component login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Sign-in" component={SignIn} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function SignIn() {
+  return <h1>Login component</h1>;
+}
